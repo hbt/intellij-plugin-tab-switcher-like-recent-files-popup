9160c3dc06ff2a017baaa4cb027d725fdbcc8829
hbt <hassenbentanfous@gmail.com>
2018-12-21 11:55:20 -0500

    added support across windows

--- src/SmartTabSwitcher.java
+++ src/SmartTabSwitcher.java
@@ -3,6 +3,7 @@ import com.intellij.ide.actions.Switcher;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
 import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
 import com.intellij.openapi.fileEditor.impl.EditorWindow;
 import com.intellij.openapi.project.Project;
@@ -10,44 +11,49 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.tabs.JBTabs;
 import org.apache.log4j.Level;
 
-public class SmartTabSwitcher extends ShowRecentlyEditedFilesAction {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SmartTabSwitcher extends AnAction {
     private final static Logger logger = LoggerFactory.createLogger();
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         logger.setLevel(Level.ALL);
-        logger.info("asd");
-        
+        logger.info("--");
+
         Project project = e.getProject();
-        if(project == null) 
-        {
+        if (project == null) {
             logger.debug("No project");
             return;
         }
 
-        // TODO(hbt) NEXT add filter for same window only -- new command
-        // TODO(hbt) NEXT get all windows
-        
-            EditorWindow editorWindow = EditorWindow.DATA_KEY.getData(e.getDataContext());
+        // TODO(hbt) NEXT add filter for same window only -- new action
+//         current editor only
+//        EditorWindow editorWindow = EditorWindow.DATA_KEY.getData(e.getDataContext());
+
+        FileEditorManagerEx manager = FileEditorManagerEx.getInstanceEx(e.getProject());
+        ArrayList<VirtualFile> tabFiles = new ArrayList<VirtualFile>();
+
+        logger.debug("WINDOWS COUNT: " + manager.getWindows().length);
+
+        for (EditorWindow editorWindow : manager.getWindows()) {
             JBTabs tabs = editorWindow.getTabbedPane().getTabs();
-//            tabs.findInfo()
             VirtualFile[] files = editorWindow.getFiles();
-            VirtualFile[] tabFiles = new VirtualFile[tabs.getTabCount()];
             for (VirtualFile f : files) {
                 if (tabs.findInfo(f) != null) {
-                    logger.debug(tabs.findInfo(f).getText());
-                    tabFiles[tabs.getIndexOf(tabs.findInfo(f))] = f;
+                    tabFiles.add(f);
                 }
             }
+        }
 
+        logger.debug("TABS COUNT: " + tabFiles.size());
+        VirtualFile[] tabFiles2 = new VirtualFile[tabFiles.size()];
+        tabFiles2 = tabFiles.toArray(tabFiles2);
+        Switcher.SwitcherPanel recently_edited_files = Switcher.createAndShowSwitcher(e, "Current Tabs", true, tabFiles2);
 
-            logger.debug("COUNT" + tabFiles.length);
-            Switcher.SwitcherPanel recently_edited_files = Switcher.createAndShowSwitcher(e, "Current Tabs", true, tabFiles);
-
-        // TODO(hbt) NEXT rm
-//        if (project != null) {
-//            Switcher.SwitcherPanel current_tabs = Switcher.createAndShowSwitcher(e, "Current Tabs", true, IdeDocumentHistory.getInstance(project).getChangedFiles());
-//        }
-        
+        // Note(hbt) list count should be shorter if we have opened windows or split tabs as the "active/visible"  are not added to the list 
+        logger.debug("LIST COUNT: " + recently_edited_files.getSelectedList().getItemsCount());
     }
 }
