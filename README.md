8e39d86f81994a49beddebdcf4decb4983070ede
hbt <hassenbentanfous@gmail.com>
2018-12-21 11:21:57 -0500

    fixed logger

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tab Switcher Like Recent Files Popup
+
+Adds a tab switcher that behaves like Recent Files or Recent Edited files popup in order to use key completion
+
+
+## Why?
+
+Current tab switcher disappears too quickly when using Ctrl+Tab. 
+You can hold the key and press Tab to move up/down the list of tabs but it is not enough.
+
+This reuses the same Switcher as `Recent files` which allows the user to press a key to quickly select tab or filter. 
+
+
--- src/SmartTabSwitcher.java
+++ src/SmartTabSwitcher.java
@@ -1,16 +1,53 @@
+import com.intellij.ide.actions.ShowRecentlyEditedFilesAction;
 import com.intellij.ide.actions.Switcher;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
+import com.intellij.openapi.fileEditor.impl.EditorWindow;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.tabs.JBTabs;
+import org.apache.log4j.Level;
 
-public class SmartTabSwitcher extends AnAction {
+public class SmartTabSwitcher extends ShowRecentlyEditedFilesAction {
+    private final static Logger logger = LoggerFactory.createLogger();
 
     @Override
     public void actionPerformed(AnActionEvent e) {
+        logger.setLevel(Level.ALL);
+        logger.info("asd");
+        
         Project project = e.getProject();
-        if (project != null) {
-            Switcher.SwitcherPanel current_tabs = Switcher.createAndShowSwitcher(e, "Current Tabs", true, IdeDocumentHistory.getInstance(project).getChangedFiles());
+        if(project == null) 
+        {
+            logger.debug("No project");
+            return;
         }
+
+        // TODO(hbt) NEXT add filter for same window only -- new command
+        // TODO(hbt) NEXT get all windows
+        
+            EditorWindow editorWindow = EditorWindow.DATA_KEY.getData(e.getDataContext());
+            JBTabs tabs = editorWindow.getTabbedPane().getTabs();
+//            tabs.findInfo()
+            VirtualFile[] files = editorWindow.getFiles();
+            VirtualFile[] tabFiles = new VirtualFile[tabs.getTabCount()];
+            for (VirtualFile f : files) {
+                if (tabs.findInfo(f) != null) {
+                    logger.debug(tabs.findInfo(f).getText());
+                    tabFiles[tabs.getIndexOf(tabs.findInfo(f))] = f;
+                }
+            }
+
+
+            logger.debug("COUNT" + tabFiles.length);
+            Switcher.SwitcherPanel recently_edited_files = Switcher.createAndShowSwitcher(e, "Current Tabs", true, tabFiles);
+
+        // TODO(hbt) NEXT rm
+//        if (project != null) {
+//            Switcher.SwitcherPanel current_tabs = Switcher.createAndShowSwitcher(e, "Current Tabs", true, IdeDocumentHistory.getInstance(project).getChangedFiles());
+//        }
+        
     }
 }
